8e92331fe75cc9b5c9f7d8b2a7c32e2f0605e5a8
Harriet Chepngeno <harrietseroney2023@gmail.com>
2025-03-16 17:18:31 +0300

    arrow function test

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = (x,y) => x/y;
+const square = (x) => x**2;
+const add = (x,y) => x+y;
+console.log(divide(2000,100));
+console.log(square(2000));
+console.log(square(100));
+console.log(add(2000,100));
